37cefa02cd9f5b02f6afa95023aab16fb427aa98
Administrator <Administrator@WIN-ODPGGT0FSKC>
2025-03-02 17:24:17 +0100

    ChangeDodaj pliki projektów.

---
+++ Fajrant-ModHelper/ModCommand.cs
@@ -0,0 +1,52 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FajrantModHelper.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class ModCommand : ICommand
+    {
+        public string Command => "mod";
+        public string[] Aliases => new string[] { };
+        public string Description => "Zmienia moderatora i gracza na Tutoriala.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (sender is not CommandSender commandSender || !commandSender.CheckPermission("mod.helper"))
+            {
+                response = "Nie masz uprawnień do użycia tej komendy!";
+                return false;
+            }
+
+            if (arguments.Count < 1 || !int.TryParse(arguments.At(0), out int targetId) || !Player.TryGet(targetId, out Player target))
+            {
+                response = "Użycie: mod <id_gracza>";
+                return false;
+            }
+
+            Player moderator = Player.List.FirstOrDefault(p => p.UserId == commandSender.SenderId);
+            if (moderator == null)
+            {
+                response = "Nie udało się znaleźć moderatora!";
+                return false;
+            }
+
+            Plugin.Instance.PlayerBackup[moderator.UserId] = new PlayerBackupData(moderator);
+            Plugin.Instance.PlayerBackup[target.UserId] = new PlayerBackupData(target);
+
+            moderator.Role.Set(RoleTypeId.Tutorial);
+            target.Role.Set(RoleTypeId.Tutorial);
+
+            Plugin.Instance.MonitoredPlayers.Add(target);
+            target.Broadcast(10, "<color=red>Moderator chce z Tobą porozmawiać. Opuszczenie serwera skutkuje automatycznym banem!</color>");
+
+            response = "Gracz został przeniesiony na Tutoriala!";
+            return true;
+        }
+    }
+}
---
+++ Fajrant-ModHelper/Plugin.cs
@@ -0,0 +1,76 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FajrantModHelper
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "FajrantModHelper";
+        public override string Author => "ItsAlex";
+        public override Version Version => new(1, 0, 6);
+        public static Plugin Instance { get; private set; }
+
+        public HashSet<string> BannedPlayers { get; } = new();
+        public HashSet<Player> MonitoredPlayers { get; } = new();
+        public HashSet<string> RecentlyBannedPlayers { get; } = new();
+        public Dictionary<string, PlayerBackupData> PlayerBackup { get; } = new();
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            Log.Info("ModHelper enabled!");
+            Exiled.Events.Handlers.Player.Left += OnPlayerLeft;
+            Exiled.Events.Handlers.Player.Banned += OnPlayerBanned;
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            Log.Info("ModHelper disabled!");
+            Exiled.Events.Handlers.Player.Left -= OnPlayerLeft;
+            Exiled.Events.Handlers.Player.Banned -= OnPlayerBanned;
+            MonitoredPlayers.Clear();
+            BannedPlayers.Clear();
+            RecentlyBannedPlayers.Clear();
+            PlayerBackup.Clear();
+        }
+
+        private void OnPlayerBanned(BannedEventArgs ev)
+        {
+            if (ev.Target != null)
+            {
+                RecentlyBannedPlayers.Add(ev.Target.UserId);
+            }
+            else
+            {
+                Log.Warn("OnPlayerBanned: ev.Target is null, skipping...");
+            }
+        }
+
+        private void OnPlayerLeft(LeftEventArgs ev)
+        {
+            if (RecentlyBannedPlayers.Remove(ev.Player.UserId)) return;
+
+            if (ev.Player.Role.Type == RoleTypeId.Tutorial && MonitoredPlayers.Contains(ev.Player))
+            {
+                if (BannedPlayers.Add(ev.Player.UserId))
+                {
+                    BanPlayer(ev.Player);
+                    Log.Info($"Gracz {ev.Player.Nickname} ({ev.Player.UserId}) został automatycznie zbanowany na 30 dni za opuszczenie serwera.");
+                }
+                MonitoredPlayers.Remove(ev.Player);
+                PlayerBackup.Remove(ev.Player.UserId);
+            }
+        }
+
+        private void BanPlayer(Player player)
+        {
+            BanManager.OfflineBanPlayer(BanHandler.BanType.UserId, player.UserId, "Ucieczka - Odwołać możesz się na Discordzie", TimeSpan.FromDays(30), player.Nickname);
+            BanManager.OfflineBanPlayer(BanHandler.BanType.IP, player.IPAddress, "Ucieczka - Odwołać możesz się na Discordzie", TimeSpan.FromDays(30), player.Nickname);
+        }
+    }
+}
